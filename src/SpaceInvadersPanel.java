c48cda55291f3e8aff641979dccebba7b5a97a79
NECKTER <nick.scheele@yahoo.com>
2015-04-15 11:55:19 -0700

    doesn't matter

--- src/SpaceInvadersPanel.java
+++ src/SpaceInvadersPanel.java
@@ -1,26 +1,58 @@
+import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class SpaceInvadersPanel extends JPanel implements ActionListener{
 	private Timer gameTimer;
 	private int numClicks;
+	private int dx = 1;
+	private int dy = 1;
+	private int x;
+	private int y;
 	
 	public SpaceInvadersPanel(){
-		super();
+		this.setPreferredSize(new Dimension(1000,800));
 		gameTimer = new Timer(5, this);
-		numClicks = 0;
+		gameTimer.start();
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
-		numClicks++;
+		moveEverything();
+		checkForCollision();
+		repaint();
 	}
 
 	
+	private void moveEverything() {
+		// TODO Auto-generated method stub
+		numClicks++;
+		if(numClicks %5==0) {
+			x += dx;
+			y += dy;
+		}
+		if(numClicks %500 == 0) {
+			y/=2;
+		}
+	}
+
 	public void checkForCollision(){
-		
+		if(x > this.getWidth() || x < 0){
+			dx *= -1;
+		}
+		if(y > this.getHeight() || y < 0){
+			dy *= -1;
+		}
+	}
+	
+	@Override
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		g.drawString(""+numClicks, x, y);
 	}
 }
