a1617e232837897d138a72daf6986e8902e5c037
Siddharth1025 <sabhansali@gmail.com>
2015-04-14 17:44:57 -0700

    Update SpaceInvadersPanel.java

--- src/SpaceInvadersPanel.java
+++ src/SpaceInvadersPanel.java
@@ -10,6 +10,9 @@ public class SpaceInvadersPanel extends JPanel implements ActionListener{
 	private Timer gameTimer;
 	private int numClicks;
 	private int dx = 1;
+	private int dy = 1;
+	private int x;
+	private int y;
 	
 	public SpaceInvadersPanel(){
 		this.setPreferredSize(new Dimension(1000,800));
@@ -29,10 +32,22 @@ public class SpaceInvadersPanel extends JPanel implements ActionListener{
 	private void moveEverything() {
 		// TODO Auto-generated method stub
 		numClicks++;
+		if(numClicks %5==0) {
+			x += dx;
+			y += dy;
+		}
+		if(numClicks %500 == 0) {
+			y/=2;
+		}
 	}
 
 	public void checkForCollision(){
-		
+		if(x > this.getWidth() || x < 0){
+			dx *= -1;
+		}
+		if(y > this.getHeight() || y < 0){
+			dy *= -1;
+		}
 	}
 	
 	@Override
