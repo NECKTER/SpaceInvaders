0f06ee5a2e578ea0344ad09222b4b3fe4bdd281c
Siddharth1025 <sabhansali@gmail.com>
2015-04-14 17:36:14 -0700

    added some stuff to the panel file

--- src/SpaceInvadersPanel.java
+++ src/SpaceInvadersPanel.java
@@ -1,26 +1,42 @@
+import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class SpaceInvadersPanel extends JPanel implements ActionListener{
 	private Timer gameTimer;
 	private int numClicks;
+	private int dx = 1;
 	
 	public SpaceInvadersPanel(){
-		super();
+		this.setPreferredSize(new Dimension(1000,800));
 		gameTimer = new Timer(5, this);
-		numClicks = 0;
+		gameTimer.start();
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
-		numClicks++;
+		moveEverything();
+		checkForCollision();
+		repaint();
 	}
 
 	
+	private void moveEverything() {
+		// TODO Auto-generated method stub
+		numClicks++;
+	}
+
 	public void checkForCollision(){
 		
+	}
+	
+	@Override
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
 	}
 }
