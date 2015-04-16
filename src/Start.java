2956038c183da691aa4801c2e4f3a5e15906c0e7
Siddharth1025 <sabhansali@gmail.com>
2015-04-16 10:46:10 -0700

    made the frame into variable

--- src/Start.java
+++ src/Start.java
@@ -1,6 +1,7 @@
 
 public class Start {
-public static void main(String[] args) {
-	new SpaceFrame().setVisible(true);
-}
+	public static void main(String[] args) {
+		SpaceFrame main = new SpaceFrame();
+		main.setVisible(true);
+	}
 }
