80c1c6c813f16cb6dc3186493e55838ccea67bb5
Siddharth1025 <sabhansali@gmail.com>
2015-04-15 19:16:17 -0700

    Update SpaceInvadersPanel.java
    fixed a bunch of errors, made the code more organized

--- src/SpaceInvadersPanel.java
+++ src/SpaceInvadersPanel.java
@@ -14,14 +14,15 @@ public class SpaceInvadersPanel extends JPanel implements ActionListener{
 	private int dx = 1;
 	private int x;
 	private int y = 50;
-	private List<Point> enemyLocs = new ArrayList<Point>();
+	private Point[] enemyLocs;
 	
 	public SpaceInvadersPanel(){
 		this.setPreferredSize(new Dimension(1000,800));
 		gameTimer = new Timer(3, this);
 		gameTimer.start();
+		enemyLocs = new Point[55];
 		for(int i = 0; i < 55; i++){
-			enemyLocs.add(new Point(1,1));
+			enemyLocs[i] = (new Point(1,1));
 		}
 	}
 
@@ -49,66 +50,54 @@ public class SpaceInvadersPanel extends JPanel implements ActionListener{
 		}
 	}
 	
+	private void enemies(Graphics g){
+		int unitNum = 0;
+		for (int i = 0; i < 11; i++){
+			Point current = enemyLocs[unitNum];
+			if (current != null){
+				g.drawString("E", x + (50 * i), y);
+				enemyLocs[unitNum] = new Point(x + (50 * i), y);
+			}
+			unitNum++;
+		}
+		for (int i = 0; i < 11; i++){
+			Point current = enemyLocs[unitNum];
+			if (current != null){
+				g.drawString("E", x + (50 * i), y + 50);
+				enemyLocs[unitNum] = new Point(x + (50 * i), y + 50);
+			}
+			unitNum++;
+		}
+		for (int i = 0; i < 11; i++){
+			Point current = enemyLocs[unitNum];
+			if (current != null){
+				g.drawString("E", x + (50 * i), y + 100);
+				enemyLocs[unitNum] = new Point(x + (50 * i), y + 100);
+			}
+			unitNum++;
+		}
+		for (int i = 0; i < 11; i++){
+			Point current = enemyLocs[unitNum];
+			if (current != null){
+				g.drawString("E", x + (50 * i), y + 150);
+				enemyLocs[unitNum] = new Point(x + (50 * i), y + 150);
+			}
+			unitNum++;
+		}
+		for (int i = 0; i < 11; i++){
+			Point current = enemyLocs[unitNum];
+			if (current != null){
+				g.drawString("E", x + (50 * i), y + 200);
+				enemyLocs[unitNum] = new Point(x + (50 * i), y + 200);
+			}
+			unitNum++;
+		}
+	}
+	
 	@Override
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
-		List<Point> temp = enemyLocs;
-		enemyLocs.clear();
-		int unitNum = 0;
-		for (int i = 0; i < 11; i++){
-			Point current = temp.get(unitNum);
-			if (current != null){ //check if this unit has been shot down
-				g.drawString("E", x + (50 * i), y);
-				enemyLocs.add(new Point(x + (50 * i), y));
-			}
-			else{
-				enemyLocs.set(unitNum, null);
-			}
-			unitNum++;
-		}
-		for (int i = 0; i < 11; i++){
-			Point current = temp.get(unitNum);
-			if (current != null){
-				g.drawString("E", x + (50 * i), y + 50);
-				enemyLocs.add(new Point(x + (50 * i), y + 50));
-			}
-			else{
-				enemyLocs.set(unitNum, null);
-			}
-			unitNum++;
-		}
-		for (int i = 0; i < 11; i++){
-			Point current = temp.get(unitNum);
-			if (current != null){
-				g.drawString("E", x + (50 * i), y + 100);
-				enemyLocs.add(new Point(x + (50 * i), y + 100));
-			}
-			else{
-				enemyLocs.set(unitNum, null);
-			}
-			unitNum++;
-		}
-		for (int i = 0; i < 11; i++){
-			Point current = temp.get(unitNum);
-			if (current != null){
-				g.drawString("E", x + (50 * i), y + 150);
-				enemyLocs.add(new Point(x + (50 * i), y + 150));
-			}
-			else{
-				enemyLocs.set(unitNum, null);
-			}
-			unitNum++;
-		}
-		for (int i = 0; i < 11; i++){
-			Point current = temp.get(unitNum);
-			if (current != null){
-				g.drawString("E", x + (50 * i), y + 200);
-				enemyLocs.add(new Point(x + (50 * i), y + 200));
-			}
-			else{
-				enemyLocs.set(unitNum, null);
-			}
-			unitNum++;
-		}
+		enemies(g);
+		
 	}
 }
